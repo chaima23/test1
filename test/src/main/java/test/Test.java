733f18b897e648d89281342478879e39a290e836
chaima <chaima@chaima-PC>
2014-05-28 02:50:56 +0200

    Signed-off-by: chaima <chaima@chaima-PC>

---
+++ test/src/main/java/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.Before;
+
+public class Test {
+	@Before
+public void setUp() throws Exception {
+		
+	
+		Test Test = new Test();
+	}
+	
+	
+
+}
---
+++ test/src/test/java/test/testapp.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.Assert.*;
+
+
+public class testapp {
+	@org.junit.Test
+	public void itShouldReturn1when2entred() {
+		assertEquals(1, testapp.process(2));
+	}
+	
+	
+	@org.junit.Test
+	public void itShouldReturn2when3entred() {
+		assertEquals(2, testapp.process(3));
+	}
+	public static int process(int n) {
+		if (n < 2) {
+			return n;
+		} else
+			return process(n - 1) + process(n - 2);
+
+	}
+}
